8392f1d63645b4dc1f856c647d0fbf0c387b076d
OperationSanti <128191513+OperationSanti@users.noreply.github.com>
2023-05-18 20:47:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+let againstAI = false;
+let playerX = 'Player X';
+let playerO = 'Player O';
+let board = Array(9).fill(null);
+let currentPlayer = playerX;
+let scores = {};
+scores[playerX] = 0;
+scores[playerO] = 0;
+
+document.getElementById('multiplayer').addEventListener('click', function() {
+    playerX = prompt("Enter Player 1's name:", "Player X") || 'Player X';
+    playerO = prompt("Enter Player 2's name:", "Player O") || 'Player O';
+    document.getElementById('playerX').innerText = playerX + ": 0";
+    document.getElementById('playerO').innerText = playerO + ": 0";
+    againstAI = false;
+    startGame();
+});
+
+document.getElementById('ai').addEventListener('click', function() {
+    playerX = "You";
+    playerO = "AI";
+    document.getElementById('playerX').innerText = playerX + ": 0";
+    document.getElementById('playerO').innerText = playerO + ": 0";
+    againstAI = true;
+    startGame();
+});
+
+document.getElementById('reset').addEventListener('click', resetGame);
+
+document.getElementById('menu').addEventListener('click', function() {
+    location.reload();
+});
+
+function startGame() {
+    document.getElementById('game-mode-selection').style.display = 'none';
+    document.getElementById('game').style.display = 'block';
+    createBoard();
+}
+
+function createBoard() {
+    let ticTacToeBoard = document.getElementById('tic-tac-toe-board');
+    ticTacToeBoard.innerHTML = "";
+    for (let i = 0; i < board.length; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener('click', () => {
+            if (!board[i]) {
+                board[i] = currentPlayer;
+                cell.innerText = currentPlayer === playerX ? 'X' : 'O';
+                cell.classList.add(currentPlayer === playerX ? 'x' : 'o');
+                if (checkWin(currentPlayer)) {
+                    scores[currentPlayer]++;
+                    document.getElementById(currentPlayer === playerX ? 'playerX' : 'playerO').innerText = currentPlayer + ": " + scores[currentPlayer];
+                    setTimeout(() => {
+                        alert(currentPlayer + ' wins!');
+                        resetGame();
+                    }, 100);
+                } else if (!board.includes(null)) {
+                    setTimeout(() => {
+                        alert('Draw!');
+                        resetGame();
+                    }, 100);
+                }
+                currentPlayer = currentPlayer === playerX ? playerO : playerX;
+            }
+        });
+        ticTacToeBoard.appendChild(cell);
+    }
+}
+
+function checkWin(player) {
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    return winningCombinations.some(combination => combination.every(index => board[index] === player));
+}
+
+function resetGame() {
+    board.fill(null);
+    currentPlayer = playerX;
+    let cells = Array.from(document.getElementsByClassName('cell'));
+    cells.forEach(cell => {
+        cell.innerText = '';
+        cell.classList.remove('x', 'o');
+    });
+}
+
